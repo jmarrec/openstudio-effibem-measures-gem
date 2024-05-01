53cd2633b0e7d00aa6b83df8198ada13485a400d
Julien Marrec <julien.marrec@gmail.com>
2024-05-01 22:23:59 +0200

    Rubocop it

--- lib/measures/enable_detailed_output_for_each_node_in_a_loop/measure.rb
+++ lib/measures/enable_detailed_output_for_each_node_in_a_loop/measure.rb
@@ -6,10 +6,9 @@
 
 # start the measure
 class EnableDetailedOutputForEachNodeInALoop < OpenStudio::Measure::ModelMeasure
-
   # human readable name
   def name
-    return "Enable Detailed Output for Each Node in a Loop"
+    return 'Enable Detailed Output for Each Node in a Loop'
   end
 
   # human readable description
@@ -44,11 +43,9 @@ The user is also asked to specify whether or not he wants to include the demand 
     loop_handles = OpenStudio::StringVector.new
     loop_display_names = OpenStudio::StringVector.new
 
-
     # ====================     airLoopHVAC   ====================
 
-
-    #putting model object and names into hash
+    # putting model object and names into hash
     loop_handles_args = model.getAirLoopHVACs
     loop_handles_args_hash = {}
     loop_handles_args.each do |loop_handles_arg|
@@ -56,24 +53,22 @@ The user is also asked to specify whether or not he wants to include the demand 
     end
 
     # Section title
-    loop_handles << OpenStudio::toUUID("").to_s
-    loop_display_names << "------ airLoopHVACs -------"
+    loop_handles << OpenStudio.toUUID('').to_s
+    loop_display_names << '------ airLoopHVACs -------'
 
-    #looping through sorted hash of model objects
-    loop_handles_args_hash.sort.map do |key,value|
+    # looping through sorted hash of model objects
+    loop_handles_args_hash.sort.map do |key, value|
       loop_handles << value.handle.to_s
       loop_display_names << key
     end
 
     # Seperator: a blank
-    loop_handles << OpenStudio::toUUID("").to_s
-    loop_display_names << ""
-
-
+    loop_handles << OpenStudio.toUUID('').to_s
+    loop_display_names << ''
 
     # ====================     PlantLoop   ====================
 
-    #putting model object and names into hash
+    # putting model object and names into hash
     loop_handles_args = model.getPlantLoops
     loop_handles_args_hash = {}
     loop_handles_args.each do |loop_handles_arg|
@@ -81,49 +76,46 @@ The user is also asked to specify whether or not he wants to include the demand 
     end
 
     # Section title
-    loop_handles << OpenStudio::toUUID("").to_s
-    loop_display_names << "------ plantLoops -------"
+    loop_handles << OpenStudio.toUUID('').to_s
+    loop_display_names << '------ plantLoops -------'
 
-    #looping through sorted hash of model objects
-    loop_handles_args_hash.sort.map do |key,value|
+    # looping through sorted hash of model objects
+    loop_handles_args_hash.sort.map do |key, value|
       loop_handles << value.handle.to_s
       loop_display_names << key
     end
 
-
-    #make a choice argument for the loop
-    loop = OpenStudio::Measure::OSArgument::makeChoiceArgument("loop", loop_handles, loop_display_names, true)
-    loop.setDisplayName("Select the plantLoop for which you want to report the node variables")
-    loop.setDescription("You can select a plantLoop or an airLoopHVAC.")
+    # make a choice argument for the loop
+    loop = OpenStudio::Measure::OSArgument.makeChoiceArgument('loop', loop_handles, loop_display_names, true)
+    loop.setDisplayName('Select the plantLoop for which you want to report the node variables')
+    loop.setDescription('You can select a plantLoop or an airLoopHVAC.')
     args << loop
 
-
-    include_demand_nodes = OpenStudio::Measure::OSArgument::makeBoolArgument("include_demand_nodes",true)
-    include_demand_nodes.setDisplayName("Include Demand Side nodes in the output?")
-    include_demand_nodes.setDefaultValue("false")
+    include_demand_nodes = OpenStudio::Measure::OSArgument.makeBoolArgument('include_demand_nodes', true)
+    include_demand_nodes.setDisplayName('Include Demand Side nodes in the output?')
+    include_demand_nodes.setDefaultValue('false')
     args << include_demand_nodes
 
     chs = OpenStudio::StringVector.new
-    chs << "Detailed"
-    chs << "Timestep"
-    chs << "Hourly"
-    chs << "Daily"
-    chs << "Monthly"
-    #chs << "RunPeriod"
-    reporting_frequency = OpenStudio::Measure::OSArgument::makeChoiceArgument('reporting_frequency', chs, true)
-    reporting_frequency.setDisplayName("<h3>Select a Reporting Frequency?</h3>")
-    reporting_frequency.setDefaultValue("Timestep")
+    chs << 'Detailed'
+    chs << 'Timestep'
+    chs << 'Hourly'
+    chs << 'Daily'
+    chs << 'Monthly'
+    # chs << "RunPeriod"
+    reporting_frequency = OpenStudio::Measure::OSArgument.makeChoiceArgument('reporting_frequency', chs, true)
+    reporting_frequency.setDisplayName('<h3>Select a Reporting Frequency?</h3>')
+    reporting_frequency.setDefaultValue('Timestep')
     args << reporting_frequency
 
     # Add a delimiter for clarify
-    delimiter = OpenStudio::Measure::OSArgument::makeStringArgument('delimiter', false)
-    delimiter.setDisplayName("<hr><h3>Select the reporting variables you want to include</h3>")
-    delimiter.setDefaultValue("-----------------------------------------------------------------")
+    delimiter = OpenStudio::Measure::OSArgument.makeStringArgument('delimiter', false)
+    delimiter.setDisplayName('<hr><h3>Select the reporting variables you want to include</h3>')
+    delimiter.setDefaultValue('-----------------------------------------------------------------')
     args << delimiter
 
-
     # Reporting variables
-    varhash =  {'System Node Temperature' => true,
+    varhash = { 'System Node Temperature' => true,
                 'System Node Setpoint Temperature' => true,
                 'System Node Mass Flow Rate' => true,
 
@@ -145,20 +137,16 @@ The user is also asked to specify whether or not he wants to include the demand 
                 'System Node Height' => false }
 
     varhash.each do |k, v|
-
-      newarg = OpenStudio::Measure::OSArgument::makeBoolArgument(k, true)
+      newarg = OpenStudio::Measure::OSArgument.makeBoolArgument(k, true)
       newarg.setDisplayName(k)
       newarg.setDefaultValue(v)
       args << newarg
-
     end
-
 
     return args
   end
 
-
-  def addNodeNamestoArray(model_objects, node_names)
+  def add_node_names_to_array(model_objects, node_names)
     model_objects.each do |model_object|
       unless model_object.to_Node.empty?
         model_node = model_object.to_Node.get
@@ -170,7 +158,6 @@ The user is also asked to specify whether or not he wants to include the demand 
     return node_names
   end
 
-
   # define what happens when the measure is run
   def run(model, runner, user_arguments)
     super(model, runner, user_arguments)
@@ -180,48 +167,45 @@ The user is also asked to specify whether or not he wants to include the demand 
       return false
     end
 
-
     # get the Loop
-    loop = runner.getOptionalWorkspaceObjectChoiceValue("loop", user_arguments, model)
+    loop = runner.getOptionalWorkspaceObjectChoiceValue('loop', user_arguments, model)
 
     # check the zone for reasonableness
     if loop.empty?
-      handle = runner.getStringArgumentValue("loop",user_arguments)
+      handle = runner.getStringArgumentValue('loop', user_arguments)
       if handle.empty?
-        runner.registerError("No plantLoop or airLoop was selected.")
+        runner.registerError('No plantLoop or airLoop was selected.')
       else
         runner.registerError("The selected loop type with handle '#{handle}' was not found in the model. It may have been removed by another measure.")
       end
       return false
     else
       # If its an airLoopHVAC
-      if not loop.get.to_AirLoopHVAC.empty?
-        #If everything's alright, get the actual Thermal Zone object from the handle
+      if !loop.get.to_AirLoopHVAC.empty?
+        # If everything's alright, get the actual Thermal Zone object from the handle
         loop = loop.get.to_AirLoopHVAC.get
-        is_airLoopHVAC = true
+        is_airloophvac = true
         runner.registerInfo("Found the airLoopHVAC surface #{loop.name}")
-      elsif not loop.get.to_PlantLoop.empty?
+      elsif !loop.get.to_PlantLoop.empty?
         loop = loop.get.to_PlantLoop.get
-        is_airLoopHVAC = false
+        is_airloophvac = false
         runner.registerInfo("Found the plantLoop #{loop.name}")
       else
-        runner.registerError("Script Error - argument not showing up as plantLoop or airLoopHVAC. Did you select the delimiter?")
+        runner.registerError('Script Error - argument not showing up as plantLoop or airLoopHVAC. Did you select the delimiter?')
         return false
       end
     end
 
-
-    include_demand_nodes = runner.getBoolArgumentValue("include_demand_nodes",user_arguments)
+    include_demand_nodes = runner.getBoolArgumentValue('include_demand_nodes', user_arguments)
 
     if include_demand_nodes
-      runner.registerInfo("Demand Nodes will be included. This can lead to many nodes.")
+      runner.registerInfo('Demand Nodes will be included. This can lead to many nodes.')
     end
 
-    reporting_frequency = runner.getStringArgumentValue("reporting_frequency",user_arguments)
-
+    reporting_frequency = runner.getStringArgumentValue('reporting_frequency', user_arguments)
 
     # Retrieve variables
-    varhash =  {'System Node Temperature' => true,
+    varhash = { 'System Node Temperature' => true,
                 'System Node Setpoint Temperature' => true,
                 'System Node Mass Flow Rate' => true,
                 'System Node Humidity Ratio' => false,
@@ -242,15 +226,15 @@ The user is also asked to specify whether or not he wants to include the demand 
                 'System Node Height' => false }
 
     variable_names = []
-    varhash.each do |k, v|
-      temp_var = runner.getBoolArgumentValue(k,user_arguments)
+    varhash.each_key do |k|
+      temp_var = runner.getBoolArgumentValue(k, user_arguments)
       if temp_var
         variable_names << k
       end
     end
 
-    if variable_names.size == 0
-      runner.registerError("You need to select at least one output variable")
+    if variable_names.empty?
+      runner.registerError('You need to select at least one output variable')
       return false
     end
 
@@ -259,33 +243,31 @@ The user is also asked to specify whether or not he wants to include the demand 
     runner.registerInitialCondition("The model started out with #{init_number_variables} output variables")
 
     runner.registerInfo("<b style='color: #00529B;background-color:#BDE5F8;'>#{variable_names.size} variables have been requested:</b>\n* #{variable_names.join("\n* ")}\n")
-    puts "Here are the variable you have selected:"
+    puts 'Here are the variable you have selected:'
     puts variable_names
-
-
 
     # Get 'em Nodes!
     # Idd type for Nodes
-    node_type = OpenStudio::Model::Node::iddObjectType
+    node_type = OpenStudio::Model::Node.iddObjectType
 
     # Create an array to store the node names
     node_names = []
 
     # Add the Supply objects
     supply_objects = loop.supplyComponents(node_type)
-    node_names = addNodeNamestoArray(supply_objects, node_names)
+    node_names = add_node_names_to_array(supply_objects, node_names)
 
     # If an AirLoop, Also add the Outside Air System Nodes
-    if is_airLoopHVAC
+    if is_airloophvac
       oa_objects = loop.oaComponents(node_type)
-      node_names = addNodeNamestoArray(oa_objects, node_names)
+      node_names = add_node_names_to_array(oa_objects, node_names)
     end
 
     # If you want to include the Demand side nodes
     if include_demand_nodes
       # Get all the demand components of type node
       dm_objects = loop.demandComponents(node_type)
-      node_names = addNodeNamestoArray(dm_objects, node_names)
+      node_names = add_node_names_to_array(dm_objects, node_names)
     end
 
     # Make it unique, just in case (shouldn't be a problem at all)
@@ -295,23 +277,21 @@ The user is also asked to specify whether or not he wants to include the demand 
     runner.registerInfo("<b style='color: #00529B;background-color:#BDE5F8;'>We found #{node_names.size} corresponding Nodes.</b>\n")
     runner.registerInfo("<b>==> #{node_names.size} nodes x #{variable_names.size} variables = <span style='color:#862d2d'>#{num_var_to_create} variables to create.</span></b>\n")
 
-
-    #Add the output variables
-    node_names.each { |node_name|
+    # Add the output variables
+    node_names.each do |node_name|
       runner.registerInfo("Enabling variables for Node #{node_name}")
       puts "\n\n==================== Node: #{node_name} ===================="
-      variable_names.each { |variable_name|
-        outputVariable = OpenStudio::Model::OutputVariable.new(variable_name,model)
-        outputVariable.setReportingFrequency(reporting_frequency)
-        outputVariable.setKeyValue(node_name)
-        puts outputVariable
+      variable_names.each do |variable_name|
+        out_var = OpenStudio::Model::OutputVariable.new(variable_name, model)
+        out_var.setReportingFrequency(reporting_frequency)
+        out_var.setKeyValue(node_name)
+        puts out_var
         puts "\n"
-      }
-    }
-
+      end
+    end
 
     num_var_created = model.getOutputVariables.size - init_number_variables
-    if num_var_created = num_var_to_create
+    if (num_var_created = num_var_to_create)
       runner.registerFinalCondition("<b style='color: #4F8A10;background-color:#DFF2BF;'>The model has now #{model.getOutputVariables.size} output variables.</b> #{num_var_created} variables were added.\n")
     else
       runner.registerFinalCondition("The model has now #{model.getOutputVariables.size} output variables")
@@ -320,11 +300,8 @@ The user is also asked to specify whether or not he wants to include the demand 
 
     # Report final condition of model
 
-
     return true
-
   end
-
 end
 
 # register the measure to be used by the application
--- lib/measures/enable_detailed_output_for_each_node_in_a_loop/tests/enable_detailed_output_for_each_node_in_a_loop_test.rb
+++ lib/measures/enable_detailed_output_for_each_node_in_a_loop/tests/enable_detailed_output_for_each_node_in_a_loop_test.rb
@@ -4,18 +4,17 @@
 require 'openstudio'
 require 'openstudio/ruleset/ShowRunnerOutput'
 require 'minitest/autorun'
-require_relative '../measure.rb'
+require_relative '../measure'
 require 'fileutils'
 require 'json'
 
 class EnableDetailedOutputForEachNodeInALoopTest < Minitest::Test
-
   def setup
     # load the test model
     translator = OpenStudio::OSVersion::VersionTranslator.new
-    path = OpenStudio::Path.new(File.dirname(__FILE__) + "/2plantloops_oneairloop_named_nodes.osm")
+    path = OpenStudio::Path.new("#{File.dirname(__FILE__)}/2plantloops_oneairloop_named_nodes.osm")
     model = translator.loadModel(path)
-    assert((not model.empty?))
+    assert(!model.empty?)
     model = model.get
     return model
   end
@@ -30,34 +29,33 @@ class EnableDetailedOutputForEachNodeInALoopTest < Minitest::Test
     # make an empty model
     model = OpenStudio::Model::Model.new
 
-    argnames = ["loop",
-                "include_demand_nodes",
-                "reporting_frequency",
-                "delimiter",
-                "System Node Temperature",
-                "System Node Setpoint Temperature",
-                "System Node Mass Flow Rate",
-                "System Node Humidity Ratio",
-                "System Node Setpoint High Temperature",
-                "System Node Setpoint Low Temperature",
-                "System Node Setpoint Humidity Ratio",
-                "System Node Setpoint Minimum Humidity Ratio",
-                "System Node Setpoint Maximum Humidity Ratio",
-                "System Node Relative Humidity",
-                "System Node Pressure",
-                "System Node Standard Density Volume Flow Rate",
-                "System Node Current Density Volume Flow Rate",
-                "System Node Current Density",
-                "System Node Enthalpy",
-                "System Node Wetbulb Temperature",
-                "System Node Dewpoint Temperature",
-                "System Node Quality",
-                "System Node Height"]
+    argnames = ['loop',
+                'include_demand_nodes',
+                'reporting_frequency',
+                'delimiter',
+                'System Node Temperature',
+                'System Node Setpoint Temperature',
+                'System Node Mass Flow Rate',
+                'System Node Humidity Ratio',
+                'System Node Setpoint High Temperature',
+                'System Node Setpoint Low Temperature',
+                'System Node Setpoint Humidity Ratio',
+                'System Node Setpoint Minimum Humidity Ratio',
+                'System Node Setpoint Maximum Humidity Ratio',
+                'System Node Relative Humidity',
+                'System Node Pressure',
+                'System Node Standard Density Volume Flow Rate',
+                'System Node Current Density Volume Flow Rate',
+                'System Node Current Density',
+                'System Node Enthalpy',
+                'System Node Wetbulb Temperature',
+                'System Node Dewpoint Temperature',
+                'System Node Quality',
+                'System Node Height']
 
     # get arguments and test that they are what we are expecting
     arguments = measure.arguments(model)
     assert_equal(argnames.size, arguments.size)
-
 
     argnames.each_with_index do |name, i|
       assert_equal(name, arguments[i].name)
@@ -65,7 +63,6 @@ class EnableDetailedOutputForEachNodeInALoopTest < Minitest::Test
   end
 
   def run_one_json_test(args_hash, output)
-
     # create an instance of the measure
     measure = EnableDetailedOutputForEachNodeInALoop.new
 
@@ -73,7 +70,7 @@ class EnableDetailedOutputForEachNodeInALoopTest < Minitest::Test
     runner = OpenStudio::Ruleset::OSRunner.new
 
     # load the test model
-    model = setup()
+    model = setup
 
     init_number_variables = model.getOutputVariables.size
 
@@ -88,15 +85,14 @@ class EnableDetailedOutputForEachNodeInALoopTest < Minitest::Test
     # populate argument with specified hash value if specified
     arguments.each do |arg|
       temp_arg_var = arg.clone
-      if args_hash.has_key?(arg.name)
+      if args_hash.key?(arg.name)
         puts "Argument name: #{arg.name}"
         assert(temp_arg_var.setValue(args_hash[arg.name]))
       end
       argument_map[arg.name] = temp_arg_var
     end
 
-
-    puts "ARGUMENT MAP"
+    puts 'ARGUMENT MAP'
     puts argument_map
 
     # run the measure
@@ -107,39 +103,34 @@ class EnableDetailedOutputForEachNodeInALoopTest < Minitest::Test
     show_output(result)
 
     # assert that it ran correctly
-    assert_equal(output["result"], result.value.valueName)
+    assert_equal(output['result'], result.value.valueName)
 
     # If there's a need to check the number of variables created, do so
-    if output.has_key?("num_var_created")
-      num_var_created =  model.getOutputVariables.size - init_number_variables
-      assert_equal(output["num_var_created"], num_var_created)
+    if output.key?('num_var_created')
+      num_var_created = model.getOutputVariables.size - init_number_variables
+      assert_equal(output['num_var_created'], num_var_created)
     end
     puts "Number of info: #{result.info.size}"
     puts "Number of warnings: #{result.warnings.size}"
-
   end
 
   def test_all_cases
-
     tests_json = {}
-    File.open(File.dirname(__FILE__) + '/test_cases.json', 'r:UTF-8') do |f|
-      tests_json = JSON.load(f)
+    File.open("#{File.dirname(__FILE__)}/test_cases.json", 'r:UTF-8') do |f|
+      tests_json = JSON.parse(f)
     end
 
-    puts "########################################"
-    puts "Test cases to run:"
-    puts tests_json.keys()
+    puts '########################################'
+    puts 'Test cases to run:'
+    puts tests_json.keys
     puts "########################################\n\n"
-
 
     tests_json.each do |k, v|
       outcome = v['output']['result']
-      puts "===================================================="
+      puts '===================================================='
       puts "Running test #{k} with expected outcome = #{outcome}"
       run_one_json_test(v['args_hash'], v['output'])
       puts "\n\n\n"
     end
-
   end
-
 end
